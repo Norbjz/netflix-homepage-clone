8c023d548dd01960780611b1a3f119ea10657ccc
Norberto Jiménez <norberto.j90@gmail.com>
2020-10-22 22:14:38 -0500

    Añadido codigo a JS

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var coll = document.getElementsByClassName("collapsible");
-var active = 0
+var activas = 0
 
 for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
+    coll[i].addEventListener("click", expandir);
+}
+
+function expandir() {
+    const activeElement = document.querySelector('.active');
+
+    if(activeElement){
+        activeElement.nextElementSibling.style.maxHeight = null;
+        activeElement.classList.remove('active')
+    }
+
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
 }
